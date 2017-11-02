2cd18cf6df4fe41dd745dd6d90c3587e3353e90d
tangjialiang <phx108@sina.com>
2017-11-02 09:48:05 +0800

    add info by tjlcast

--- src/main/java/cn/edu/bupt/App.java
+++ src/main/java/cn/edu/bupt/App.java
@@ -2,7 +2,6 @@ package cn.edu.bupt;
 
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
-import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
